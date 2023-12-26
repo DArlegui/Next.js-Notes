c31b119c3d5f450f87a94689db51e60b31bf4854
dinden <arleguidaniel@gmail.com>
2023-12-26 23:21:59 +0900

    Catch-all Segments

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {
+  params: { slug: string[] };
+}
+
+export default function ProductPage({ params: { slug } }: Props) {
+  return <div>ProductPage {slug}</div>;
+}
