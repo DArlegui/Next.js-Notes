05b57f6b428d51ae8281e48eb95125ab7d10e89c
dinden <arleguidaniel@gmail.com>
2023-12-30 07:31:06 +0900

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,12 @@ export function GET(request: NextRequest) {
     { id: 2, name: 'Uto' },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  //Validation
+  if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
+
+//Use Postman for testing
