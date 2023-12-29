0b6462f7b2b26426a53fa01adb5b8f7df781773b
dinden <arleguidaniel@gmail.com>
2023-12-30 08:14:25 +0900

    Deleting an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,28 +1,27 @@
-import next from 'next';
 import { NextRequest, NextResponse } from 'next/server';
+//URL: http://localhost:3000/api/users/#
 
+//GET for getting a list of objects
 export function GET(request: NextRequest, { params }: { params: { id: number } }) {
-  //Fetch data from a db
-  //If not found, return 404 error
-  //Else return data
   if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
   return NextResponse.json({ id: 1, name: 'Daniel' });
 }
 
-//URL: http://localhost:3000/api/users/11
-
 //PUT for replacing an object
 //PATCH for updating an object
 export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
-  //Validate the request body
-  //If invalid, return 400 error
-  //Fetch the user with the given id
-  //If doesn't exist, return 404 error
-  //Else update the user
-  //Return the updated user
-
   const body = await request.json();
   if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 });
   if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+//DELETE for deleting an object
+export function DELETE(request: NextRequest, { params }: { params: { id: number } }) {
+  //Fetch user from db
+  //If not found, return 404
+  //Delete the user
+  //Return 200
+  if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  return NextResponse.json({});
 }
