20d3aeaa11fe73bfb97693438245ec430f19b7f5
dinden <arleguidaniel@gmail.com>
2023-12-30 07:38:42 +0900

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,19 @@ export function GET(request: NextRequest, { params }: { params: { id: number } }
 }
 
 //URL: http://localhost:3000/api/users/11
+
+//PUT for replacing an object
+//PATCH for updating an object
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+  //Validate the request body
+  //If invalid, return 400 error
+  //Fetch the user with the given id
+  //If doesn't exist, return 404 error
+  //Else update the user
+  //Return the updated user
+
+  const body = await request.json();
+  if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+  if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+}
