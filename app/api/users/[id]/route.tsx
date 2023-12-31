ca5e1680b32d5c369073260a4e09e07eb2349d61
dinden <arleguidaniel@gmail.com>
2023-12-31 16:16:44 +0900

    Deleting Data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -33,11 +33,10 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
 }
 
 //DELETE for deleting an object
-export function DELETE(request: NextRequest, { params }: { params: { id: number } }) {
-  //Fetch user from db
-  //If not found, return 404
-  //Delete the user
-  //Return 200
-  if (params.id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  const user = await prisma.user.findUnique({ where: { id: parseInt(params.id) } });
+  if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  await prisma.user.delete({ where: { id: user.id } });
   return NextResponse.json({});
 }
