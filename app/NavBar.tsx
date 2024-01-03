8a0809d8d8396ea437f652edd861351839282c52
dinden <arleguidaniel@gmail.com>
2024-01-03 20:56:31 +0900

    Signing Out Users

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,7 +10,14 @@ const NavBar = () => {
       <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
       {status === 'loading' && <div>Loading...</div>}
-      {status === 'authenticated' && <div>{session.user!.name}</div>}
+      {status === 'authenticated' && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Logout
+          </Link>
+        </div>
+      )}
       {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
     </div>
   );
