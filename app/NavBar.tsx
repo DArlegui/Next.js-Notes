e74a88038571d69b81e5b439b725e534671c77f7
dinden <arleguidaniel@gmail.com>
2024-01-03 15:30:25 +0900

    Configured Google Provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,11 +2,10 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 justify-center p-5 text-black font-semibold">
-      <Link href="/" className="mr-5">
-        Next.js
-      </Link>
+    <div className="flex bg-slate-200 justify-center p-5 text-black font-semibold space-x-3">
+      <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
