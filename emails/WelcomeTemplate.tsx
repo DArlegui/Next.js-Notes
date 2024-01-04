bb194a2b4d6432cd4e51ce96842e97a8bed5fd9c
dinden <arleguidaniel@gmail.com>
2024-01-04 14:44:31 +0900

    Styling Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,17 +1,30 @@
-import { Html, Body, Container, Text, Link, Preview } from '@react-email/components';
+import { Html, Body, Container, Tailwind, Text, Link, Preview } from '@react-email/components';
+// import { CSSProperties } from 'react'; //Another way
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://darlegui-mini-portfolio.netlify.app/">https://darlegui-mini-portfolio.netlify.app/</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://darlegui-mini-portfolio.netlify.app/">
+              https://darlegui-mini-portfolio.netlify.app/
+            </Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
+
+// const body: CSSProperties = {
+//   background: '#fff',
+// };
+
+// const heading: CSSProperties = {
+//   fontSize: '32px',
+// };
 
 export default WelcomeTemplate;
