11cb7b31afd45b3dbe9088045bd3f97682d438a1
dinden <arleguidaniel@gmail.com>
2024-01-04 13:21:07 +0900

    Creating an Email Template

---
+++ email/WelcomeTemplate.tsx
@@ -0,0 +1,17 @@
+import { Html, Body, Container, Text, Link, Preview } from '@react-email/components';
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://darlegui-mini-portfolio.netlify.app/">My Website</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
